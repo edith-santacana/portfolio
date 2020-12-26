855ac6a48c2320d0511bef1179fe4518002d912a
Edith Santacana <edith.santacana@gmail.com>
2020-12-25 23:01:53 -0800

    Add banner

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<Banner class="full-screen" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Banner from './components/Banner.vue';
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+	components: {
+		Banner,
+	},
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+  height: 100%;
+	width: 100%;
+}
+
+html,
+body {
+	height: 100vh;
+	width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+
+.full-screen {
+	height: 100%;
+	width: 100%;
 }
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="banner-container">
+		<div class="title">
+			<div class="text">Hi!</div>
+		</div>
+		<background class="background"></background>
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Background from './Background.vue';
+
+@Options({
+	components: { Background },
+	props: {},
+})
+export default class Banner extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.banner-container {
+	position: relative;
+	background: blue;
+	height: 100%;
+	width: 100%;
+}
+
+.background {
+	position: absolute;
+	height: 100%;
+	width: 100%;
+}
+
+.title {
+	color: white;
+	position: absolute;
+	height: 100%;
+	width: 100%;
+	z-index: 2;
+	display: flex;
+}
+
+.text {
+	justify-content: center;
+	margin: auto;
+	font-weight: 700;
+	font-size: 4em;
+	text-shadow: rgb(202 66 66 / 79%) 6px 4px 11px;
+}
+</style>
